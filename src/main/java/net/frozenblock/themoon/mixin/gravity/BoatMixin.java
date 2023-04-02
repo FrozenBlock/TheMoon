1ca9834769c71cf923e45587cfd28030fe5be25c
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-04-02 19:53:03 -0400

    TNT & Boats use gravity

---
+++ src/main/java/net/frozenblock/themoon/mixin/gravity/BoatMixin.java
@@ -0,0 +1,21 @@
+package net.frozenblock.themoon.mixin.gravity;
+
+import net.frozenblock.themoon.util.gravity.impl.EntityGravityInterface;
+import net.minecraft.world.entity.item.PrimedTnt;
+import net.minecraft.world.entity.vehicle.Boat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(Boat.class)
+public class BoatMixin {
+
+	@ModifyArgs(method = "floatBoat", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;setDeltaMovement(DDD)V", ordinal = 0))
+	public void theMoon$useGravity(Args args) {
+		double y = (double)args.get(1) + (double)0.04F;
+		Boat boat = Boat.class.cast(this);
+		args.set(1, y -(double)((EntityGravityInterface)boat).getEffectiveGravity());
+	}
+
+}
---
+++ src/main/java/net/frozenblock/themoon/mixin/gravity/PrimedTntMixin.java
@@ -0,0 +1,20 @@
+package net.frozenblock.themoon.mixin.gravity;
+
+import net.frozenblock.themoon.util.gravity.impl.EntityGravityInterface;
+import net.minecraft.world.entity.item.PrimedTnt;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(PrimedTnt.class)
+public class PrimedTntMixin {
+
+	@ModifyArgs(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;setDeltaMovement(DDD)V", ordinal = 0))
+	public void theMoon$useGravity(Args args) {
+		double y = (double)args.get(1) + (double)0.04F;
+		PrimedTnt primedTnt = PrimedTnt.class.cast(this);
+		args.set(1, y -(double)((EntityGravityInterface)primedTnt).getEffectiveGravity());
+	}
+
+}
