e30ea91f192c6a7388823e8b8618c311d853d38f
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-04-02 18:31:37 -0400

    update noise

--- src/main/java/net/frozenblock/themoon/registry/TheMoonNoiseGeneratorSettings.java
+++ src/main/java/net/frozenblock/themoon/registry/TheMoonNoiseGeneratorSettings.java
@@ -1,28 +1,59 @@
 package net.frozenblock.themoon.registry;
 
 import java.util.List;
+import java.util.stream.Stream;
 import net.frozenblock.themoon.util.TheMoonSharedConstants;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.levelgen.DensityFunction;
 import net.minecraft.world.level.levelgen.DensityFunctions;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.NoiseRouter;
 import net.minecraft.world.level.levelgen.NoiseRouterData;
 import net.minecraft.world.level.levelgen.NoiseSettings;
+import net.minecraft.world.level.levelgen.Noises;
+import net.minecraft.world.level.levelgen.OreVeinifier;
+import net.minecraft.world.level.levelgen.synth.NormalNoise;
 
 public class TheMoonNoiseGeneratorSettings {
 
 	public static final ResourceKey<NoiseGeneratorSettings> MOON = ResourceKey.create(Registries.NOISE_SETTINGS, TheMoonSharedConstants.id("the_moon"));
 
-	public static final NoiseSettings MOON_NOISE_SETTINGS = NoiseSettings.create(0, 256, 2, 1);
+	public static final NoiseSettings MOON_NOISE_SETTINGS = NoiseSettings.create(0, 208, 2, 1);
 
-	protected static NoiseRouter moonNoiseRouter(HolderGetter<DensityFunction> holderGetter) {
-		DensityFunction densityFunction = NoiseRouterData.postProcess(slideMoon(NoiseRouterData.getFunction(holderGetter, NoiseRouterData.DEPTH)));
-		return new NoiseRouter(DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero(), slideMoon(DensityFunctions.constant(-0.703125)), densityFunction, DensityFunctions.zero(), DensityFunctions.zero(), DensityFunctions.zero());
+	protected static NoiseRouter moonNoiseRouter(HolderGetter<DensityFunction> densityGetter, HolderGetter<NormalNoise.NoiseParameters> noiseGetter) {
+		DensityFunction densityFunction = NoiseRouterData.postProcess(slideMoon(NoiseRouterData.getFunction(densityGetter, NoiseRouterData.DEPTH)));
+		int i = Stream.of(OreVeinifier.VeinType.values()).mapToInt(veinType -> 2).min().orElse(-DimensionType.MIN_Y * 2);
+		int j = Stream.of(OreVeinifier.VeinType.values()).mapToInt(veinType -> 40).max().orElse(-DimensionType.MIN_Y * 2);
+		DensityFunction Y = NoiseRouterData.getFunction(densityGetter, ResourceKey.create(Registries.DENSITY_FUNCTION, new ResourceLocation("y")));
+		return new NoiseRouter(
+				DensityFunctions.zero(), //BARRIER NOISE
+				DensityFunctions.zero(), //FLUID LEVEL FLOODEDNESS NOISE
+				DensityFunctions.zero(), //FLUID LEVEL SPREAD NOISE
+				DensityFunctions.zero(), //LAVA NOISE
+				DensityFunctions.zero(), //TEMPERATURE
+				DensityFunctions.zero(), //VEGETATION
+				DensityFunctions.zero(), //CONTINENTS
+				DensityFunctions.zero(), //EROSION
+				DensityFunctions.zero(), //DEPTH
+				NoiseRouterData.getFunction(densityGetter, NoiseRouterData.RIDGES), //RIDGES
+				slideMoon(DensityFunctions.constant(-0.703125)), //INITIAL DENSITY WITHOUT JADDEGNESS
+				densityFunction, //FINAL DENSITY
+				yLimitedInterpolatable(Y, DensityFunctions.noise(noiseGetter.getOrThrow(Noises.ORE_VEININESS), 1.5, 1.5), i, j, 0), //VEIN TOGGLE
+				DensityFunctions.add(
+						DensityFunctions.constant(-0.08f),
+						DensityFunctions.max(
+								yLimitedInterpolatable(Y, DensityFunctions.noise(noiseGetter.getOrThrow(Noises.ORE_VEIN_A), 4.0, 4.0), i, j, 0).abs(),
+								yLimitedInterpolatable(Y, DensityFunctions.noise(noiseGetter.getOrThrow(Noises.ORE_VEIN_B), 4.0, 4.0), i, j, 0).abs()
+						)
+				), //VEIN RIDGED
+				DensityFunctions.noise(noiseGetter.getOrThrow(Noises.ORE_GAP)) //VEIN GAP
+		);
 	}
 
 	private static DensityFunction slideMoon(DensityFunction densityFunction) {
@@ -34,6 +65,10 @@ public class TheMoonNoiseGeneratorSettings {
 	}
 
 	private static NoiseGeneratorSettings moon(BootstapContext<?> bootstapContext) {
-		return new NoiseGeneratorSettings(MOON_NOISE_SETTINGS, Blocks.END_STONE.defaultBlockState(), Blocks.AIR.defaultBlockState(), moonNoiseRouter(bootstapContext.lookup(Registries.DENSITY_FUNCTION)), TheMoonSurfaceRules.moon(), List.of(), 0, false, false, true, false);
+		return new NoiseGeneratorSettings(MOON_NOISE_SETTINGS, Blocks.END_STONE.defaultBlockState(), Blocks.AIR.defaultBlockState(), moonNoiseRouter(bootstapContext.lookup(Registries.DENSITY_FUNCTION), bootstapContext.lookup(Registries.NOISE)), TheMoonSurfaceRules.moon(), List.of(), 0, false, false, true, false);
+	}
+
+	private static DensityFunction yLimitedInterpolatable(DensityFunction input, DensityFunction whenInRange, int minInclusive, int maxInclusive, int maxRange) {
+		return DensityFunctions.interpolated(DensityFunctions.rangeChoice(input, minInclusive, maxInclusive + 1, whenInRange, DensityFunctions.constant(maxRange)));
 	}
 }
