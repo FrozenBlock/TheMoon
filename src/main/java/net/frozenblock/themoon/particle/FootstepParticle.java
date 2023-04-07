de2a234a2234fa2ca13eae58efa05d3d43312ccb
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-04-07 18:59:38 -0400

    optimize footstep particle rendering a bit

--- src/main/java/net/frozenblock/themoon/particle/FootstepParticle.java
+++ src/main/java/net/frozenblock/themoon/particle/FootstepParticle.java
@@ -13,12 +13,15 @@ import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 @Environment(EnvType.CLIENT)
 public class FootstepParticle extends Particle {
+	private static final float radians = (float)Math.PI / 180F;
+	
 	private final TextureAtlasSprite sprite;
 	private final float rot;
 
@@ -27,7 +30,7 @@ public class FootstepParticle extends Particle {
 		this.xd = 0.0;
 		this.yd = 0.0;
 		this.zd = 0.0;
-		this.rot = (float)g;
+		this.rot = radians * (float)g;
 		this.lifetime = 200;
 		this.gravity = 0.0f;
 		this.hasPhysics = false;
@@ -49,26 +52,19 @@ public class FootstepParticle extends Particle {
 
 	@Override
 	public void render(VertexConsumer buffer, Camera renderInfo, float partialTicks) {
-		float f = ((float)this.age + partialTicks) / (float)this.lifetime;
-		f *= f;
-		float g = 2.0f - f * 2.0f;
-		g *= 0.2f;
-		float h = 0.125f;
+		float alpha = Mth.lerp(((float)this.age + partialTicks) / (float)this.lifetime, 1F, 0F);
 		Vec3 vec3 = renderInfo.getPosition();
-		float i = (float)(this.x - vec3.x);
-		float j = (float)(this.y - vec3.y);
-		float k = (float)(this.z - vec3.z);
 		int l = this.getLightColor(partialTicks);
-		float m = this.sprite.getU0();
-		float n = this.sprite.getU1();
-		float o = this.sprite.getV0();
-		float p = this.sprite.getV1();
-		Matrix4f matrix4f = new Matrix4f().translation(i, j, k);
-		matrix4f.rotate((float)Math.PI / 180 * this.rot, 0.0f, 1.0f, 0.0f);
-		buffer.vertex(matrix4f, -0.125f, 0.0f, 0.125f).uv(m, p).color(this.rCol, this.gCol, this.bCol, g).uv2(l).endVertex();
-		buffer.vertex(matrix4f, 0.125f, 0.0f, 0.125f).uv(n, p).color(this.rCol, this.gCol, this.bCol, g).uv2(l).endVertex();
-		buffer.vertex(matrix4f, 0.125f, 0.0f, -0.125f).uv(n, o).color(this.rCol, this.gCol, this.bCol, g).uv2(l).endVertex();
-		buffer.vertex(matrix4f, -0.125f, 0.0f, -0.125f).uv(m, o).color(this.rCol, this.gCol, this.bCol, g).uv2(l).endVertex();
+		float u0 = this.sprite.getU0();
+		float u1 = this.sprite.getU1();
+		float v0 = this.sprite.getV0();
+		float v1 = this.sprite.getV1();
+		Matrix4f matrix4f = new Matrix4f().translation((float)(this.x - vec3.x), (float)(this.y - vec3.y), (float)(this.z - vec3.z));
+		matrix4f.rotate(this.rot, 0.0f, 1.0f, 0.0f);
+		buffer.vertex(matrix4f, -0.125f, 0.0f, 0.125f).uv(u0, v1).color(this.rCol, this.gCol, this.bCol, alpha).uv2(l).endVertex();
+		buffer.vertex(matrix4f, 0.125f, 0.0f, 0.125f).uv(u1, v1).color(this.rCol, this.gCol, this.bCol, alpha).uv2(l).endVertex();
+		buffer.vertex(matrix4f, 0.125f, 0.0f, -0.125f).uv(u1, v0).color(this.rCol, this.gCol, this.bCol, alpha).uv2(l).endVertex();
+		buffer.vertex(matrix4f, -0.125f, 0.0f, -0.125f).uv(u0, v0).color(this.rCol, this.gCol, this.bCol, alpha).uv2(l).endVertex();
 	}
 
 	@Environment(value=EnvType.CLIENT)
